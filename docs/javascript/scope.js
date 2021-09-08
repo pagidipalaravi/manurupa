8ae9092c9e034909d9ec63cc315436dbf390b44e
pagidipalaravi <pagidipala.ravi@gmail.com>
2021-09-08 12:28:54 +0530

    ravi
    ravi

--- docs/javascript/scope.js
+++ docs/javascript/scope.js
@@ -1,35 +1,43 @@
-carName1 = "benz";// variables declared outside the function is called global scope.
-var carName2 = "eco";// variables declared outside the function is called global scope
-let carName3 = "bmw";// variables declared outside the function is called global scope
+/* Global scope varaibles */
+g1 = 1; 	//global scope undefined type variable.
+var g2 = 2; //global scope var type variable.
+let g3 = 3; //global scope let type variable.
 function onLoad() {
-    // this is block scope example.
-    {
-        x = 2; //block scope undclared number type variable.
-        var y = 3;// block scope var type variable
-        let z = 4;// block scope let type variable
-        console.log("x=" + x);//we can acess block level undeclared variables inside the block.
-        console.log("y=" + y);//we can acess block level var type variable inside the block.
-        console.log("z=" + z);// we can acess block level let type variable inside the block.
-        console.log("carName1=" + carName1);
-        console.log("carName2=" + carName2);
-        console.log("carName3=" + carName3);
-    }
-    console.log("x=" + x); //we can acess block level undeclared variables outside of the block.
-    console.log("y=" + y); // we can acess block level var type variables outside of the block.
-    console.log("z=" + z);// we cannot access let variable outside of the block.
-
-    scopeFunction();
-
+	/* Function scope variables */
+	f1 = 4; 	// function scope undefined type variable.
+	var f2 = 5; // function scope var type variable
+	let f3 = 6; // function scope let type variable
+	{
+		/* Block scope variables */
+		b1 = 7; 	//block scope undefined type variable.
+		var b2 = 8; // block scope var type variable
+		let b3 = 9; // block scope let type variable
+		console.log("g1=" + g1);
+		console.log("g2=" + g2);
+		console.log("g3=" + g3);
+		console.log("f1=" + f1);
+		console.log("f2=" + f2);
+		console.log("f3=" + f3);
+		console.log("b1=" + b1); //we can acess block level undefined type variable inside the block.
+		console.log("b2=" + b2); //we can acess block level var type variable inside the block.
+		console.log("b3=" + b3); //we can acess block level let type variable inside the block.
+	}
+	console.log("g1=" + g1);
+	console.log("g2=" + g2);
+	console.log("g3=" + g3);
+	console.log("f1=" + f1);
+	console.log("f2=" + f2);
+	console.log("f3=" + f3);
+	console.log("b1=" + b1); //we can acess block level undefined variable outside of the block.
+	console.log("b2=" + b2); //we can acess block level var type variable outside of the block.
+	console.log("b3=" + b3); //we cannot access  block level let type variable outside of the block.
 }
-function scopeFunction() {
-    var carName = "Volvo";
-    console.log(typeof carName + " " + carName);
-}
-console.log(typeof carName + " " + carName);
-//you cannot acceces the carName because the car name in the function scope.
-//you will get an error  Uncaught ReferenceError: carName is not defined
-
-console.log("I can display " + carName1);// this is global scope example
-console.log("I can display " + carName2);//  this is global scope example.
-console.log("I can display " + carName3);// this is globel scope example.
-
+console.log("g1=" + g1);
+console.log("g2=" + g2);
+console.log("g3=" + g3);
+console.log("f1=" + f1);
+console.log("f2=" + f2);
+console.log("f3=" + f3);
+console.log("b1=" + b1);
+console.log("b2=" + b2);
+console.log("b3=" + b3);
