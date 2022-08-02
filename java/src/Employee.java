eb4a6aa228aedcd60538a18c9d7171f9e46508bb
pagidipalaravi <pagidipala.ravi@gmail.com>
2022-08-02 17:53:03 +0530

    ravi changes

---
+++ java/src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("Employee class");
+	}
+
+public class Group {
+		public static void main(String[] args) {
+			System.out.println("Group class");
+		}
+	}
+
+}
